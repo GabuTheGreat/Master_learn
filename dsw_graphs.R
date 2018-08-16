27d967c8a66a3d52a4d9779e96ac555d1ef9ec39
Gabriel Mutua <gabrielndunda@gmail.com>
2018-08-16 17:04:56 +0300

    Added few graphs

--- dsw_graphs.R
+++ dsw_graphs.R
@@ -1,4 +1,19 @@
 library(dplyr)
+library(ggplot2)
 
 #load data
-march_april <- read.csv("~/learn/dsw_data.csv", header = TRUE)
+all_data <- read.csv("~/learn/dsw_data.csv", header = TRUE)
+
+#sample graphs Kenya
+#graphs data
+graph_data <- all_data %>% filter(Country == "Kenya", Year == 2018) %>% 
+              select(Period, TC_Adoption,Region)
+
+#plotting simple graph
+ggplot(data = graph_data, aes(x = Region, y = TC_Adoption, fill = Period))+
+         geom_bar(stat = "identity", position = position_dodge())
+
+#plotting simple line graph
+ggplot(data = graph_data, aes(x = Period, y = TC_Adoption, group = Region))+
+  geom_line(aes(color = Region))+
+  geom_point(aes(color = Region))
